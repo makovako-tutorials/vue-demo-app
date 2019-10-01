73d14153cf25b3047268c9f7fc305d943cd55fae
Marek Valko <valko.marek@gmail.com>
2019-10-01 22:13:48 +0200

    Innitial Commit

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,46 @@
+<template>
+  <form @submit="addTodo">
+    <input type="text" name="title" v-model="title" placeholder="Add Todo..." />
+    <input type="submit" value="Submit" class="btn" />
+  </form>
+</template>
+
+<script>
+// import uuid from 'uuid';
+export default {
+  name: "AddTodo",
+  data() {
+      return {
+          title: ''
+      }
+  },
+  methods: {
+      addTodo(e){
+          e.preventDefault();
+          const newTodo = {
+            //   id: uuid.v4(),
+              title: this.title,
+              completed: false
+          }
+            //send up to parent
+        this.$emit('add-todo', newTodo);
+        this.title='';
+      }
+  }
+};
+</script>
+
+<style scope>
+form {
+  display: flex;
+}
+
+input[type="text"] {
+  flex: 10;
+  padding: 5px;
+}
+
+input[type="submit"] {
+  flex: 2;
+}
+</style>
